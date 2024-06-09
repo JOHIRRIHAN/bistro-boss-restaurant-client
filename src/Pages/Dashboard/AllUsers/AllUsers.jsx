4386c2925250169921f2dec64463ef3390102db3
JOHIR RIHAN <jr7718301@gmail.com>
2024-06-09 18:44:09 +0600

    Display All Users And Create Make Admin API

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -3,23 +3,57 @@ import SectionTitlr from "../../../Components/SectionTitle/SectionTitlr";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import { MdDelete } from "react-icons/md";
 import { FaUsers } from "react-icons/fa";
+import Swal from "sweetalert2";
+// import useCart from "../../../Hooks/useCart";
 
 const AllUsers = () => {
+    // const [ refetch] = useCart();
   const axiosSecure = useAxiosSecure();
-  const { data: users = [] } = useQuery({
+  const { data: users = [], refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
       const res = await axiosSecure.get("/users");
       return res.data;
     },
   });
+
+  const handleMakeAdmin = user =>{
+    
+  }
+
+
+  const handleDeleteUser = user =>{
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          axiosSecure.delete(`/users/${user._id}`).then((res) => {
+            if (res.data.deletedCount > 0) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success",
+              });
+              refetch()
+            }
+          });
+        }
+      });
+
+  }
   return (
     <div>
       <SectionTitlr
         heading={"MANAGE ALL USERS"}
         subHeading={"How many??"}
       ></SectionTitlr>
-      <div className="shadow-2xl shadow-black p-5 ">
+      <div className="shadow-2xl shadow-black p-5 max-w-screen-lg mx-auto">
         <div>
           {" "}
           <h3 className="text-3xl font-semibold mb-5">
@@ -45,8 +79,8 @@ const AllUsers = () => {
                         <th>{index + 1}</th>
                         <td>{user.name}</td>
                         <td>{user.email}</td>
-                        <td><button className="btn text-2xl text-white bg-[#D1A054]"><FaUsers></FaUsers></button></td>
-                        <td><button className="btn text-2xl text-white bg-red-700"><MdDelete /></button></td>
+                        <td><button onClick={()=> handleMakeAdmin(user)} className="btn text-2xl text-white bg-[#D1A054]"><FaUsers></FaUsers></button></td>
+                        <td><button onClick={()=> handleDeleteUser(user)} className="btn text-2xl text-white bg-red-700"><MdDelete /></button></td>
                       </tr>)
                 }
               </tbody>
